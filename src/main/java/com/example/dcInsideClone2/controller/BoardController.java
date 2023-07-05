71d992b4bf32fe010a8c9de515c02b9f1c8b58b4
LeeJaeHyeok97 <hazardous10@naver.com>
2023-07-06 02:51:12 +0900

    게시글 목록

--- src/main/java/com/example/dcInsideClone2/controller/BoardController.java
+++ src/main/java/com/example/dcInsideClone2/controller/BoardController.java
@@ -4,10 +4,13 @@ import com.example.dcInsideClone2.dto.BoardDTO;
 import com.example.dcInsideClone2.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,5 +28,13 @@ public class BoardController {
         System.out.println("boardDTO = " + boardDTO);
         boardService.save(boardDTO);
         return "index";
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) { //데이터 전체를 가져와야 할 때는 Model 객체를 사용한다.
+        //DB에서 전체 게시글 데이터를 가져와서 list.html에 보여준다.
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
     }
 }
--- src/main/java/com/example/dcInsideClone2/service/BoardService.java
+++ src/main/java/com/example/dcInsideClone2/service/BoardService.java
@@ -9,6 +9,9 @@ import com.example.dcInsideClone2.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BoardService {
@@ -17,5 +20,15 @@ public class BoardService {
     public void save(BoardDTO boardDTO) {
         BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
         boardRepository.save(boardEntity);
+    }
+
+    public List<BoardDTO> findAll() { //엔티티로 받아온 객체를 DTO로 변환해서 옮겨줘야한다.
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        //엔티티 객체를 DTO객체로 옮겨 담는다.
+        for (BoardEntity boardEntity : boardEntityList) {
+            boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
+        }
+
     }
 }
